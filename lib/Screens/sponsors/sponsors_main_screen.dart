524a2fee8570af5dd58666f942463a63980c5e25
ARKO BANDYOPADHYAY <100148599+arkobandyopadhyay@users.noreply.github.com>
2023-02-04 01:07:33 +0530

    sponsors,gallery data done

--- lib/Screens/sponsors/sponsors_main_screen.dart
+++ lib/Screens/sponsors/sponsors_main_screen.dart
@@ -14,15 +14,16 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
   double screenWidth = 0;
 
   bool startAnimation = false;
-
   List<String> texts = [
-    "Microsoft",
-    "Amazon",
-    "Adobe",
-    "Flipkart",
-    "Netfix",
-    "Facebook",
-    "Samsung",
+    "Inside me",
+    "Sparkcars",
+    "LIC",
+    "Press Information bureau",
+    "Rebounce Raipur",
+    "AB Classes",
+    "Raag-The music academy",
+    "Raag music cafe",
+    "Sargam musicals"
   ];
   @override
   void initState() {
@@ -34,32 +35,34 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor:Color(0xff000000),
-          title: Text('Sponsors',style: const TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.w500,
-              color: Color(0xffFFEBC1),
-            ),),
-          leading: IconButton(
-            onPressed: ()=>ZoomDrawer.of(context)!.toggle(), 
-            icon: Icon(Icons.menu)
+      appBar: AppBar(
+        backgroundColor: Color(0xff000000),
+        title: Text(
+          'Sponsors',
+          style: const TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w500,
+            color: Color(0xffFFEBC1),
           ),
         ),
+        leading: IconButton(
+            onPressed: () => ZoomDrawer.of(context)!.toggle(),
+            icon: Icon(Icons.menu)),
+      ),
       body: Stack(
         children: [
           Container(
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("asset/menu/sponsors_jungle2.jpg"),
-                fit: BoxFit.cover,
-              )
-            ),
+                image: DecorationImage(
+              image: AssetImage("asset/welcomeCarousel/5.png"),
+              fit: BoxFit.cover,
+            )),
           ),
           SafeArea(
             child: SingleChildScrollView(
@@ -69,7 +72,9 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
               ),
               child: Column(
                 children: [
-                  const SizedBox(height: 40,),
+                  const SizedBox(
+                    height: 40,
+                  ),
                   ListView.builder(
                     primary: false,
                     shrinkWrap: true,
@@ -78,7 +83,9 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
                       return item(index);
                     },
                   ),
-                  const SizedBox(height: 50,),
+                  const SizedBox(
+                    height: 50,
+                  ),
                 ],
               ),
             ),
@@ -87,13 +94,15 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
       ),
     );
   }
+
   Widget item(int index) {
     return AnimatedContainer(
-      height: 70,
+      height: 100,
       width: screenWidth,
       curve: Curves.easeInOut,
       duration: Duration(milliseconds: 300 + (index * 200)),
-      transform: Matrix4.translationValues(startAnimation ? 0 : screenWidth, 0, 0),
+      transform:
+          Matrix4.translationValues(startAnimation ? 0 : screenWidth, 0, 0),
       margin: const EdgeInsets.only(
         bottom: 12,
       ),
@@ -101,28 +110,40 @@ class _SponsorsMainScreenState extends State<SponsorsMainScreen> {
         horizontal: screenWidth / 20,
       ),
       decoration: BoxDecoration(
-        color: Color(0xff3C2317),
+        color: Color(0xffD7A86E),
         borderRadius: BorderRadius.circular(10),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // CircleAvatar(
-          //   child: Image.asset("assets/dinosaur_ready.png"),
-          // ),
-          Align(
-          alignment: Alignment.center,
-          child: Text(
-            "${texts[index]}",
-            style: const TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w500,
-              color: Color(0xffFFEBC1),
+          Container(
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("asset/menu/human.jpg"),
+                  radius: 40,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text: texts[index],
+                        style: TextStyle(fontSize: 18), // default text style
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),),
+          )
         ],
       ),
     );
   }
-
 }
